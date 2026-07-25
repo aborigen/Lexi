9f68cd89940a99a1359ab1f4d1b4748dba16455e
egaborigen <egaborigen@gmail.com>
2026-07-25 07:21:24 +0000

    refactor word levels to store data in json files for English and Russian

--- src/lib/levels.ts
+++ src/lib/levels.ts
@@ -1,14 +1,14 @@
 import { WordLevel } from './levels/types';
-import { LEVELS_EN } from './levels/en';
-import { LEVELS_RU } from './levels/ru';
+import enLevels from './levels/en.json';
+import ruLevels from './levels/ru.json';
 
 export type { WordLevel };
 
 /**
- * @fileOverview Unified export for all game levels.
+ * @fileOverview Unified export for all game levels, now loaded from JSON.
  */
 
 export const LEVELS: WordLevel[] = [
-  ...LEVELS_EN,
-  ...LEVELS_RU
+  ...(enLevels as WordLevel[]),
+  ...(ruLevels as WordLevel[])
 ];
